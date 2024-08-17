a0b20c8e6a21ad0f22864940fe41e4fb2944eb68
Aswin krishna <krishnaaswin415@gmail.com>
2024-08-18 01:52:43 +0530

    first commit

--- video-confer/client/src/components/VideoCall.js
+++ video-confer/client/src/components/VideoCall.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import { useState,useRef } from 'react';
+import { useState, useRef,useEffect } from 'react';
+import {Socket, io} from 'socket.io-client';
+import SimplePeer from 'simple-peer';
 import Webcam from 'react-webcam'
 
 const VideoCall = () => {
+
+  const videoRef = useRef(null)
+
+useEffect(() => {
+
+  const peer = new SimplePeer({
+    initiator: false,
+    trickle:false,
+  })
+
+peer.on('signal',(data) => {
+  Socket.emit('answer',data);
+});
+peer.on('stream',(stream) => {
+  videoRef
+})
+
+},[])
+
   return (
     <div>
         
-<video />
+
 
     </div>
   )
--- video-confer/server/index.js
+++ video-confer/server/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { PORT } from './config.js';
+import {createServer} from 'http';
 import cors from 'cors';
 import {Server} from 'socket.io'
 
@@ -11,14 +12,26 @@ const io = new Server(server);
 app.use(cors());
 
 app.get('/',(req,res) => {
-    //used socket.io for full duplex communication and also use webrtc for video and audio calling 
+    //used socket.io for full duplex communication and also use webrtc for video and audio calling  
     console.log('req');
-    return res.status(234).send('welcome')
+    return res.status(200).send('welcome')
 })
 
 io.on('connection', (socket) => {
 console.log('a user connected')
-socket.on('disconnected',() => {
+
+socket.on('offer',(data) => {
+    socket.broadcast.emit('offer',data)
+});
+
+socket.on('answer',(data) => {
+    socket.broadcast.emit('answer',data)
+});
+socket.on('ice-candidate',(data) => {
+    socket.broadcast.emit('ice-candiate',data)
+});
+
+socket.on('disconnect',() => {
     console.log('user disconnected')
 });
 
