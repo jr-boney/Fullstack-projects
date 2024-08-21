6981e98fc303b1560cea3709aadad7b8e3673a76
Aswin krishna <krishnaaswin415@gmail.com>
2024-08-21 21:02:30 +0530

    initial commit

---
+++ Book store - Copy/backend/config.js
@@ -0,0 +1,12 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 
+'mongodb+srv://aswin741269:sLxW2ydSLrDAkTiP@book-store-mern.8r8wi1j.mongodb.net/?retryWrites=true&w=majority&appName=Book-store-MERN';
+
+// export const PORT = 5555;
+
+// export const mongoDBURL =
+//   'mongodb+srv://root:root@books-store-mern.c7ba8z1.mongodb.net/books-collection?retryWrites=true&w=majority';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
