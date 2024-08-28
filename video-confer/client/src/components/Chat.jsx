fd7636670e467505ecaea6b908f282f2a54120f1
Aswin krishna <krishnaaswin415@gmail.com>
2024-08-29 01:03:50 +0530

    commit

--- video-confer/client/src/components/Chat.jsx
+++ video-confer/client/src/components/Chat.jsx
@@ -2,9 +2,18 @@ import React from "react";
 
 const Chat = () => {
   return (
-    <div className="w-1/4 h-3/4 bg-white rounded-l-md shadow-lg p-4 flex flex-col justify-between absolute right-0 top-10 bottom-0">
-      <div className="overflow-y-auto flex-grow mb-4">
-        <input />
+    <div className="w-1/4 h-3/4 bg-white rounded-l-md p-4 flex flex-col justify-between absolute right-0 top-10 bottom-0 shadow-xl">
+      <div className="overflow-y-auto flex items-end ml-5 flex-grow mb-4">
+        <div className="relative">
+          <input
+            className="h-7 w-3/4 bg-gray-100 border-2 border-gray-300 text-gray-900 placeholder-gray-500 rounded-md p-2"
+            type="text"
+            placeholder="Write here"
+          />
+          <button className="bg-blue-500 text-white rounded-md px-2 ml-3 py-1">
+            Send
+          </button>
+        </div>
       </div>
     </div>
   );
